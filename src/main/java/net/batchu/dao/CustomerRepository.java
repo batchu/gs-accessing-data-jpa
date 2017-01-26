5dc493e1962f90299d9d4e597307f0c7b977a74a
Prashanth Batchu <pbatchu.contractor@ofiglobal.com>
2017-01-26 12:47:28 -0500

    Clean basic Springt Boot CRUD app with Swagger and Spring Actuator

---
+++ src/main/java/net/batchu/dao/CustomerRepository.java
@@ -0,0 +1,11 @@
+package net.batchu.dao;
+
+import net.batchu.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+}
